0fc397851d2a989f25900943a23f6877fdca9d4a
Reagan Dantinaise G. Albano <qrdgalbano@tip.edu.ph>
2026-02-27 15:07:09 +0800

    February 27, 2026

---
+++ Midterms/Module_3/3.2.4_SECTION_PRACTICE/M3_SP2.js
@@ -0,0 +1,7 @@
+let length = Number(prompt("Enter length"));
+let width = Number(prompt("Enter width"));
+let height = Number(prompt("Enter height"));
+
+let volume = length * width * height;
+
+console.log(volume);
