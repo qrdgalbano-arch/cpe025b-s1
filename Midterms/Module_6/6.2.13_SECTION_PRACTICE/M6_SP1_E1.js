0d471e99575210aced4c82abf47a415fb5ae8671
Reagan Dantinaise G. Albano <qrdgalbano@tip.edu.ph>
2026-02-27 16:01:49 +0800

    February 27, 2026

---
+++ Midterms/Module_6/6.2.13_SECTION_PRACTICE/M6_SP1_E1.js
@@ -0,0 +1,5 @@
+// Exercise 1
+let end = 2; 
+for(let i=1; i <= end; i++) {
+    console.log(i); 
+}
---
+++ Midterms/Module_6/6.2.13_SECTION_PRACTICE/M6_SP1_E2.js
@@ -0,0 +1,13 @@
+// Exercise 2
+
+let counter = 0; 
+let maxValue = 10; 
+let result = 1; 
+ 
+debugger; 
+for (counter = 0; counter < maxValue; counter++) { 
+    console.log(result); 
+    result *= maxValue - counter - 1; 
+} 
+ 
+console.log("Final result: ", result);
